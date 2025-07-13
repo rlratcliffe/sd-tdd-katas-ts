92230cccc8cb4ba64a7ed4027397876cd7160132
Steven Diamante <stevendiamante13@gmail.com>
2025-07-13 14:51:32 -0400

    use recursion to process multiple commands

--- katas/mars-rover/src/MarsRover.test.ts
+++ katas/mars-rover/src/MarsRover.test.ts
@@ -1,14 +1,20 @@
-import { Commands, MissionExecution, execute, Rover } from "./MarsRover";
+import { Commands, execute, MissionExecution, Rover } from "./MarsRover";
 
 describe('Mars Rover', () => {
 
     it('should face north and be located at origin', () => {
-        verify(execute)([])({ direction: 'N', position: { x: 0, y: 0 } });
+        verify(execute)()([])({ direction: 'N', position: { x: 0, y: 0 } });
+    });
+
+    it('should move', () => {
+        verify(execute)()(['M'])({ direction: 'N', position: { x: 0, y: 1 } });
+        verify(execute)()(['M', 'M'])({ direction: 'N', position: { x: 0, y: 2 } });
     });
 });
 
 const verify = (executeFn: MissionExecution) =>
+    (rover?: Rover) =>
     (commands: Commands) =>
         (expectedRover: Rover): void => {
-            expect(executeFn(commands)).toStrictEqual(expectedRover);
+            expect(executeFn(rover)(commands)).toStrictEqual(expectedRover);
         };
--- katas/mars-rover/src/MarsRover.ts
+++ katas/mars-rover/src/MarsRover.ts
@@ -12,12 +12,31 @@ export type Rover = {
 
 export type Commands = Command[];
 
-export type MissionExecution = (commands: Commands) => Rover;
+export type MissionExecution = (rover?: Rover) => (commands: Commands) => Rover;
 
-export const execute = (commands: Commands): Rover => {
-    console.log(commands);
-    return {
-        direction: 'N',
-        position: { x: 0, y: 0 }
+const initialRover: Rover = {
+    direction: 'N',
+    position: { x: 0, y: 0 }
+};
+
+export const execute = (rover: Rover = initialRover) =>
+    (commands: Commands): Rover => {
+        if (commands.length === 0) {
+            return rover
+        }
+        const [firstCommand, ...remainingCommands] = commands;
+        const newRover = processCommand(firstCommand, rover);
+        console.log(`processed command: ${firstCommand} new rover location: ${JSON.stringify(newRover)}`);
+        return execute(newRover)(remainingCommands);
+    };
+
+const processCommand = (firstCommand: Command, rover: Rover): Rover => {
+    if (firstCommand === 'M') {
+        const newY: Coordinate = ((rover.position.y + 1) % 10) as Coordinate;
+        return {
+            direction: 'N',
+            position: { x: 0, y: newY }
+        };
     }
+    throw new Error(`Unsupported command: ${firstCommand}`);
 };
